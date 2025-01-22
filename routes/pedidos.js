f070159bd005c1344aa396254f201bef18895d4f
anacarlacastro <ana.carla.castro62@aluno.ifce.edu.br>
2025-01-22 12:05:16 -0300

    Informações de localização

--- routes/pedidos.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-// Rota para a página "pedidos"
-router.get('/', (req, res) => {
-  res.render('pedidos');  
-});
-
-module.exports = router;
